b6448412e19fa4799ed417b452f0408ff6782c5d
吳柏宗 <centerseed@hotmail.com>
2016-11-28 10:13:29 +0800

    Add crash sender application

---
+++ app/src/main/java/com/moana/roadpro_manage/LogApplication.java
@@ -0,0 +1,43 @@
+package com.moana.roadpro_manage;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.multidex.MultiDexApplication;
+
+public class LogApplication extends MultiDexApplication {
+    public void onCreate() {
+        super.onCreate();
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable e) {
+                handleUncaughtException(thread, e);
+            }
+        });
+    }
+
+    public boolean isUIThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
+
+    public void handleUncaughtException(Thread thread, Throwable e) {
+        e.printStackTrace();
+        if (isUIThread()) {
+            invokeLogActivity();
+        } else {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    invokeLogActivity();
+                }
+            });
+        }
+    }
+
+    private void invokeLogActivity() {
+        Intent intent = new Intent("com.moana.carsharing.SEND_LOG");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        System.exit(1);
+    }
+}
