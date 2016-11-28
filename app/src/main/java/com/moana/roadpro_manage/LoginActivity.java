2d56146dd4e556662b1c0d60355724a6501eba02
吳柏宗 <centerseed@hotmail.com>
2016-11-28 15:21:47 +0800

    Add login page

---
+++ app/src/main/java/com/moana/roadpro_manage/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.moana.roadpro_manage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/moana/roadpro_manage/MainActivity.java
+++ app/src/main/java/com/moana/roadpro_manage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moana.roadpro_manage;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 
@@ -202,5 +204,15 @@ public class MainActivity extends AppCompatActivity {
         mItemPlugActivity.setOnClickListener(drawerClickListener);
         mItemPlugManage.setOnClickListener(drawerClickListener);
         mItemPlugReport.setOnClickListener(drawerClickListener);
+
+        Button logout = (Button) headerView.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
